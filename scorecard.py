be1be23794ff9384da1e60e4b657a9a24e73862b
MikeBikeLA <michael.mu.sun@gmail.com>
2018-07-11 18:04:32 +0800

    adding binning for WOE

--- scorecard.py
+++ scorecard.py
@@ -9,16 +9,32 @@ app_train = pd.read_csv('input/application_train.csv')
 print('Training data shape: ', app_train.shape)
 
 app_train['DAYS_BIRTH'] = abs(app_train['DAYS_BIRTH'])  # Division results in float by default
-col_age_years = app_train['DAYS_BIRTH']/365
+age_df = pd.DataFrame()
+age_df['AGE'] = app_train['DAYS_BIRTH']/365
 # Greater negative correlation == less likely to default as value (age) increases
-print(col_age_years.corr(app_train['TARGET']))
+print(age_df['AGE'].corr(app_train['TARGET']))
 
-age_df = pd.concat([col_age_years, app_train['TARGET']], axis=1)
-print(age_df.head())
+age_df = pd.concat([age_df, app_train['TARGET']], axis=1)
+# print(age_df.head())
 
-age_df = age_df.sort_values(by=['DAYS_BIRTH'])
-print(age_df.head(20))
+age_df = age_df.sort_values(by=['AGE'])
+value_counts = age_df['TARGET'].value_counts()
+# print(value_counts)
+age_zeroes = value_counts[0]  # total zeroes
+age_ones = value_counts[1]  # total ones
 
-bins_age = pd.cut(age_df['DAYS_BIRTH'], 3)
-print(bins_age)
-print(age_df.shape)
+age_bins = np.array_split(age_df, 10)
+null_bin = pd.DataFrame()
+
+for bin in age_bins:
+    null_bin.append(bin.loc[bin['AGE'].isnull()])  # Not sure if this will catch NULL values ##PLEASE CHECK##
+    bin_min = bin['AGE'].min()
+    bin_max = bin['AGE'].max()
+    bin_value_counts = bin['TARGET'].value_counts()
+    bin_zeroes = bin_value_counts[0]
+    bin_ones = bin_value_counts[1]
+    print(bin.shape)
+
+
+def getWOE(bin):
+    return
