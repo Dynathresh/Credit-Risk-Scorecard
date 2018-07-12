db03905e6d153d687cd648e004edabbe440ca933
MikeBikeLA <michael.mu.sun@gmail.com>
2018-07-12 18:09:32 +0800

    Finished basic WOE/IV table. HAve not accounted for NULL values yet.

--- scorecard.py
+++ scorecard.py
@@ -1,6 +1,7 @@
-import numpy as np
-import pandas as pd
 import os
+import pandas as pd
+import numpy as np
+import math
 
 print(os.listdir("input/"))
 
@@ -20,21 +21,53 @@ age_df = pd.concat([age_df, app_train['TARGET']], axis=1)
 age_df = age_df.sort_values(by=['AGE'])
 value_counts = age_df['TARGET'].value_counts()
 # print(value_counts)
-age_zeroes = value_counts[0]  # total zeroes
-age_ones = value_counts[1]  # total ones
+total_non_events = value_counts[0]  # Total zeroes
+total_events = value_counts[1]  # Total ones
 
-age_bins = np.array_split(age_df, 10)
+age_bins = np.array_split(age_df, 10)  # Split into 10 equal bins
 null_bin = pd.DataFrame()
 
-for bin in age_bins:
-    null_bin.append(bin.loc[bin['AGE'].isnull()])  # Not sure if this will catch NULL values ##PLEASE CHECK##
-    bin_min = bin['AGE'].min()
-    bin_max = bin['AGE'].max()
-    bin_value_counts = bin['TARGET'].value_counts()
-    bin_zeroes = bin_value_counts[0]
-    bin_ones = bin_value_counts[1]
-    print(bin.shape)
+# woe_df = pd.DataFrame(columns=['RANGE', 'BINS', 'NON_EVENTS', 'EVENTS', 'PERCENT_NON_EVENTS', 'PERCENT_EVENTS'])
+woe_list = []  # Create list of dicts, then convert to DataFrame. Efficient because concat recreates data structure
+bin_num = 0
+for my_bin in age_bins:
+    bin_num += 1
 
+    # Not sure if these will catch NULL values ##PLEASE CHECK##---------------------------------------------------------
+    null_bin = pd.concat([null_bin, my_bin.loc[my_bin['AGE'].isnull()]], ignore_index=True)
+    # null_bin.append(my_bin.loc[my_bin['AGE'].isnull()])  # Append is supposedly slower than concat
+    my_bin = my_bin.drop(my_bin[my_bin['AGE'].isnull()].index)  # Removes null values from bin
+
+    wd = {}
+    # print('Rows: ' + str(my_bin.shape[0]/app_train.shape[0]))  # % of all rows in this bin
+
+    bin_min = my_bin['AGE'].min()  # Min bin
+    bin_max = my_bin['AGE'].max()  # Max bin
+    wd['RANGE'] = str(round(bin_min, 2)) + '-' + str(round(bin_max, 2))
+    bin_value_counts = my_bin['TARGET'].value_counts()
+    wd['NON_EVENTS'] = bin_value_counts[0]  # Non-Events in this bin
+    wd['EVENTS'] = bin_value_counts[1]  # Events in this bin
+    percent_non_events = wd['NON_EVENTS']/total_non_events
+    percent_events = wd['EVENTS']/total_events
+    wd['PERCENT_NON_EVENTS'] = str(round(percent_non_events*100, 2)) + '%'  # % of Non-Events
+    wd['PERCENT_EVENTS'] = str(round(percent_events*100, 2)) + '%'  # % of Events
+    woe = math.log(percent_non_events/percent_events)
+    IV = (percent_non_events - percent_events) * woe
+    wd['WOE'] = round(woe, 4)
+    wd['IV'] = round(IV, 4)
+    woe_list.append(wd)
+
+woe_df = pd.DataFrame(woe_list, columns=['RANGE', 'NON_EVENTS', 'EVENTS', 'PERCENT_NON_EVENTS', 'PERCENT_EVENTS', 'WOE', 'IV'])
+pd.set_option('display.expand_frame_repr', False)  # Allows DF to be displayed in full
+# IV_total = woe_df['PSEUDO_IV'].sum()
+woe_df.at['TOTAL', 'IV'] = woe_df['IV'].sum()
+print(woe_df.head(11))
+# print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' + 'IV: ' + str(IV))
+# Add IV sum to DF  # https://stackoverflow.com/questions/41286569/get-total-of-pandas-column/41286607
+# print(null_bin.head())
+
+
+# df['variance'] = df['budget'] + df['actual']
 
 def getWOE(bin):
     return
